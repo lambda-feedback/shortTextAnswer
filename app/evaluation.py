0c0fd832741a5dcf6f706848cf44c92f9bcdbb14
Codrin <cc1619@ic.ac.uk>
2023-02-10 15:06:48 +0000

    Add word frequency

--- app/evaluation.py
+++ app/evaluation.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.linalg
 
 from nltk.corpus import wordnet
+from nltk.corpus import brown
 
 def evaluation_function(response, answer, params):
     """
@@ -29,11 +30,44 @@ def evaluation_function(response, answer, params):
     to output the evaluation response.
     """
 
-    print(wordnet.synsets("test")[0])
+    similarity, response_scores, answer_scores = sentence_similarity(response, answer)
 
-    return {
-        "is_correct": True
-    }
+    if similarity > 0.8:
+        return {
+            "is_correct": True,
+            "result": {
+                "similarity_value": similarity
+            }
+        }
+
+    else:
+        dif = 0
+        word = None
+        for (resp_score, ans_score) in zip(response_scores, answer_scores):
+            if np.abs(resp_score[0] - ans_score[0]) > dif:
+                dif = np.abs(resp_score[0] - ans_score[0])
+                word = resp_score[1]
+        return {
+            "is_correct": False,
+            "result": {
+                "similarity_value": similarity,
+                "Problematic_word": word
+            }
+        }
+
+freqs = {}
+def preprocess_word_freqs():
+    for word in brown.words():
+        if not word in freqs:
+            freqs[word] = 0
+        freqs[word] = freqs[word] + 1
+
+def word_information_content(word):
+    if word not in freqs:
+        f = 0
+    else:
+        f = freqs[word]
+    return 1 - (np.log(f + 1)) / (np.log(len(brown.words()) + 1))
 
 def word_similarity(word1, word2):
     synsets1 = wordnet.synsets(word1)
@@ -42,10 +76,9 @@ def word_similarity(word1, word2):
     # Find LCA
     dist = 0
     for synset1, synset2 in zip(synsets1, synsets2):
-        if synset1.pos() is not synset2.pos():
-            continue
+        #if synset1.pos() is not synset2.pos():
+        #    continue
         dist = max(dist, synset1.wup_similarity(synset2))
-        break
     return dist
 
 def sentence_similarity(response: str, answer: str):
@@ -69,7 +102,7 @@ def sentence_similarity(response: str, answer: str):
                 if word_similarity(word, other_word) > best_similarity:
                     best_similarity = word_similarity(word, other_word)
                     best_word = other_word
-        response_scores.append(best_similarity * 1) # TODO: Use word freq
+        response_scores.append((best_similarity * word_information_content(word) * word_information_content(best_word), word))
 
     for word in all_words:
         best_similarity = 0
@@ -81,13 +114,18 @@ def sentence_similarity(response: str, answer: str):
                 if word_similarity(word, other_word) > best_similarity:
                     best_similarity = word_similarity(word, other_word)
                     best_word = other_word
-        answer_scores.append(best_similarity * 1) # TODO: Use word freq
+        answer_scores.append((best_similarity * word_information_content(word) * word_information_content(best_word), word))
 
     print(response_scores)
     print(answer_scores)
+    resp_scores = response_scores.copy()
+    ans_scores = answer_scores.copy()
+    for idx in range(len(response_scores)):
+        response_scores[idx] = response_scores[idx][0]
+        answer_scores[idx] = answer_scores[idx][0]
     score = np.dot(response_scores, answer_scores)/(np.linalg.norm(response_scores) * np.linalg.norm(answer_scores))
-    return score
+    return score, resp_scores, ans_scores
 
 if __name__ == "__main__":
-    print(evaluation_function("This is the response", "This is the answer", None))
-    print(sentence_similarity("Solid, liquid, gas", "boat, plane, volcano"))
+    preprocess_word_freqs()
+    print(evaluation_function("A list of characters", "a sequence of characters", None))
