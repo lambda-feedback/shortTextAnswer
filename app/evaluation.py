8efe1f0ecfd6f33c0951e19bb44087fe138ef86e
Codrin <cc1619@ic.ac.uk>
2023-04-03 17:53:34 +0300

    Fix dockerfile

--- app/evaluation.py
+++ app/evaluation.py
@@ -34,13 +34,6 @@ def evaluation_function(response, answer, params):
     return types and that evaluation_function() is the main function used 
     to output the evaluation response.
     """
-    return {
-        "is_correct": True,
-        "result": {
-            "similarity_value": 1
-        },
-        "feedback": "Correct!"
-    }
     similarity, response_scores, answer_scores = sentence_similarity(response, answer)
 
     if params is not None and "keywords" in params:
